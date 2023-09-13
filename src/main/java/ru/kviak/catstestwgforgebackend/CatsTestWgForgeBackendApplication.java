cee749524ec5694c1304951df3d20d903eb29517
Denis Lapin <kviakit@Gmail.com>
2023-09-13 22:49:31 +0300

    Initial commit

---
+++ src/main/java/ru/kviak/catstestwgforgebackend/CatsTestWgForgeBackendApplication.java
@@ -0,0 +1,12 @@
+package ru.kviak.catstestwgforgebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsTestWgForgeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatsTestWgForgeBackendApplication.class, args);
+    }
+}
